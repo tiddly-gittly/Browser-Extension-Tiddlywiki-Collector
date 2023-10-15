708d41db53eed8329f0929abc6ae0f954eba68a3
linonetwo <linonetwo012@gmail.com>
2023-10-16 02:41:10 +0800

    fix: host_permissions is not used

--- src/manifest.ts
+++ src/manifest.ts
@@ -25,7 +25,7 @@ const manifest = defineManifest(async (environment) => ({
       js: ['content/index.tsx'],
     },
   ],
-  host_permissions: ['<all_urls>'],
+  // host_permissions: ['<all_urls>'],
   options_ui: {
     page: 'options/options.html',
     open_in_tab: true,

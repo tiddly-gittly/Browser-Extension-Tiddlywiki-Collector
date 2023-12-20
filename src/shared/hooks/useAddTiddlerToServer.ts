170fe3695a1a4ea91e9077b010b19a8caaa9f99f
oeyoews <jyao4783@gmail.com>
2023-12-20 21:41:26 +0800

    get username

--- src/shared/hooks/useAddTiddlerToServer.ts
+++ src/shared/hooks/useAddTiddlerToServer.ts
@@ -1,55 +1,95 @@
 import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { IServerStatus } from 'tiddlywiki';
 import type { ITiddlerFieldsParam } from 'tw5-typed';
 import { Writable } from 'type-fest';
 import { addProtocolToUrl } from '../../utils';
 import { IServerInfo, ServerStatus, useServerStore } from '../server/store';
 
-export type ITiddlerToAdd = Writable<Omit<ITiddlerFieldsParam, 'created' | 'modified'>>;
+export type ITiddlerToAdd = Writable<
+  Omit<ITiddlerFieldsParam, 'created' | 'modified'>
+>;
 
 export function useAddTiddlerToServer() {
   const { t } = useTranslation();
-  const activeServers = useServerStore(({ servers }) => Object.values(servers).filter(server => server.active));
-  const onlineServers = useServerStore(({ servers }) => Object.values(servers).filter(server => server.status === ServerStatus.online));
-  const setActiveServers = useServerStore(({ setActive, servers }) => (idToActive: string[]) => {
-    Object.values(servers).forEach(server => {
-      if (idToActive.includes(server.id)) {
-        setActive(server.id, true);
-      } else {
-        setActive(server.id, false);
-      }
-    });
-  });
-  const addTiddlerToServer = useCallback(async (server: IServerInfo, tiddler: ITiddlerToAdd): Promise<void> => {
-    const putTiddlerUrl = new URL(`recipes/default/tiddlers/${tiddler.title as string}`, addProtocolToUrl(server.uri));
-    try {
-      tiddler.created = toTWUTCString(new Date());
-      tiddler.creator = t('TWCollector');
-      // Recent tab need `modified` field to work
-      tiddler.modified = toTWUTCString(new Date());
-      // user name on the view template is `modifier`
-      tiddler.modifier = t('TWCollector');
-      await fetch(putTiddlerUrl, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-requested-with': 'TiddlyWiki',
-        },
-        body: JSON.stringify({ title: tiddler.title, fields: tiddler }),
+  const activeServers = useServerStore(({ servers }) => Object.values(servers).filter((server) => server.active));
+  const onlineServers = useServerStore(({ servers }) =>
+    Object.values(servers).filter(
+      (server) => server.status === ServerStatus.online,
+    )
+  );
+  const setActiveServers = useServerStore(
+    ({ setActive, servers }) => (idToActive: string[]) => {
+      Object.values(servers).forEach((server) => {
+        if (idToActive.includes(server.id)) {
+          setActive(server.id, true);
+        } else {
+          setActive(server.id, false);
+        }
       });
-    } catch (error) {
-      console.error(`${server.name} ${t('Log.SaveFailed')} Error: ${(error as Error).message}`);
-    }
-  }, [t]);
-  const addTiddlerToAllActiveServers = useCallback(async (newTiddler: ITiddlerToAdd) => {
-    for (const server of activeServers) {
+    },
+  );
+
+  // const getusername = async () => {
+  //   const baseURL = new URL('status', addProtocolToUrl(onlineServers[0].uri));
+  //   try {
+  //     const data = await fetch(baseURL, {
+  //       method: 'GET',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'x-requested-with': 'TiddlyWiki',
+  //       },
+  //     });
+  //     const { username } = await data.json() as IServerStatus;
+  //     return username;
+  //   } catch (error) {
+  //     console.error('[获取用户名]:', baseURL, error);
+  //   }
+  // };
+
+  const addTiddlerToServer = useCallback(
+    async (server: IServerInfo, tiddler: ITiddlerToAdd): Promise<void> => {
+      const putTiddlerUrl = new URL(
+        `recipes/default/tiddlers/${tiddler.title as string}`,
+        addProtocolToUrl(server.uri),
+      );
+      // const username = await getusername();
       try {
-        await addTiddlerToServer(server, newTiddler);
+        tiddler.created = toTWUTCString(new Date());
+        // tiddler.creator = (username != null) || t('TWCollector');
+        tiddler.creator = t('TWCollector');
+        // Recent tab need `modified` field to work
+        tiddler.modified = toTWUTCString(new Date());
+        // user name on the view template is `modifier`
+        tiddler.modifier = t('TWCollector');
+        await fetch(putTiddlerUrl, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-requested-with': 'TiddlyWiki',
+          },
+          body: JSON.stringify({ title: tiddler.title, fields: tiddler }),
+        });
       } catch (error) {
-        console.error(error);
+        console.error(
+          `${server.name} ${t('Log.SaveFailed')} Error: ${(error as Error).message}`,
+        );
       }
-    }
-  }, [activeServers, addTiddlerToServer]);
+    },
+    [t],
+  );
+  const addTiddlerToAllActiveServers = useCallback(
+    async (newTiddler: ITiddlerToAdd) => {
+      for (const server of activeServers) {
+        try {
+          await addTiddlerToServer(server, newTiddler);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    },
+    [activeServers, addTiddlerToServer],
+  );
   return {
     onlineServers,
     activeServers,
@@ -66,13 +106,11 @@ export function pad(number: number) {
   return String(number);
 }
 export function toTWUTCString(date: Date) {
-  return `${date.getUTCFullYear()}${pad(date.getUTCMonth() + 1)}${pad(date.getUTCDate())}${
-    pad(
-      date.getUTCHours(),
+  return `${date.getUTCFullYear()}${pad(date.getUTCMonth() + 1)}${pad(
+    date.getUTCDate(),
+  )
+    }${pad(date.getUTCHours())}${pad(date.getUTCMinutes())}${pad(
+      date.getUTCSeconds(),
     )
-  }${pad(date.getUTCMinutes())}${pad(date.getUTCSeconds())}${
-    (date.getUTCMilliseconds() / 1000)
-      .toFixed(3)
-      .slice(2, 5)
-  }`;
+    }${(date.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5)}`;
 }

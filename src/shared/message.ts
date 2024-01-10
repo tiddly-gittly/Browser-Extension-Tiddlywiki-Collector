3379e9e1f6a072ebbba98318c03518f5b5bf72da
linonetwo <linonetwo012@gmail.com>
2024-01-10 22:53:52 +0800

    feat: add comment to message types

--- src/shared/message.ts
+++ src/shared/message.ts
@@ -3,16 +3,40 @@ import type { Image } from 'mdast';
 import { Asset } from '../popup/AssetTable';
 
 export enum ITabActions {
+  /**
+   * Given some image nodes (metadata and image url), ask content script to return the asset items, containing the actual image content.
+   */
   getAssets = 'getAssets',
+  /**
+   * Response of `getAssets`, alone with actual image content.
+   */
   getAssetsResponse = 'getAssetsResponse',
+  /**
+   * Get overall metadata and main content of the webpage.
+   */
   getReadability = 'getReadability',
+  /**
+   * Response of `getReadability`, alone with the metadata and main content of the webpage.
+   */
   getReadabilityResponse = 'getReadabilityResponse',
+  /**
+   * we will try this when user click on extension icon. (whenever first-time or second-time, just try it)
+   *
+   * If `parameter.selectedElement` exists, means this is second-time (user open popup before and choose the "select manually", which is the first time.).
+   *
+   * Also will check if there is any user selection (native selection) in the webpage. If there is, we will try to get the content of the selection.
+   */
   startClipping = 'startClipping',
+
   startClippingResponse = 'startClippingResponse',
-  startSelecting = 'startSelecting',
+  /**
+   * Start manual selection of clipping zone. Trigger by clicking 'select manually' in popup.
+   * Then user can select a dom node of the webpage, and get content of that dom node.
+   */
+  startSelectingClippingZone = 'startSelecting',
 }
 export interface IStartSelectionMessage {
-  action: ITabActions.startSelecting;
+  action: ITabActions.startSelectingClippingZone;
 }
 export interface IStartClippingMessage {
   action: ITabActions.startClipping;
